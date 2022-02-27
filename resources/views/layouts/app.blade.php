554a5015a9e11de13b980134de154c40cdac49df
Kazeem Banjoko <banjoko10@gmail.com>
2022-02-27 17:20:56 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
                             Channels
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                @foreach (App\Models\Channel::all() as $channel)
+                                @foreach ($channels as $channel)
                                     <a class="dropdown-item" href="{{ route('channels', $channel->slug) }}">{{ $channel->name }}</a>
                                 @endforeach
                             </div>
