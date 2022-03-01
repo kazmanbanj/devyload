96996ebb498ce60fdf3397be43d8624d05cfd89d
Kazeem Banjoko <banjoko10@gmail.com>
2022-03-01 13:47:57 +0100

    user profile

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Thread;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,16 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="card">
-        <div class="card-header">
             <h5>{{ $profileUser->name }}
                 <small>joined since {{ $profileUser->created_at->diffForHumans() }}</small>
             </h5>
+
+        <div class="card">
+            @foreach ($profileUser->threads as $thread)
+            <div class="card-header">
+                <a href="{{ route('profile.show', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
+                {{ $thread->title }}
+
+                <span class="float-end">
+                    {{ $thread->created_at->diffForHumans() }}
+                </span>
+            </div>
+
+            <div class="card-body">
+                <div class="body">{{ $thread->body }}</div>
+            </div>
+            @endforeach
         </div>
 
-        <div class="card-body">
-            body here...
-        </div>
-    </div>
+        {{-- {{ $threads->links() }} --}}
 </div>
 @endsection
