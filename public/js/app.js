f5f1c8ed9fbb6814272cd2a189037d0f54903a1f
Kazeem Banjoko <banjoko10@gmail.com>
2022-06-13 16:55:41 +0100

    fix thread factory issue

--- public/js/app.js
+++ public/js/app.js
@@ -6742,7 +6742,6 @@ __webpack_require__.r(__webpack_exports__);
 
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
-  // props: ['endpoint'],
   data: function data() {
     return {
       body: ''
